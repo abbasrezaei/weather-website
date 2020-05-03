cadd818a2c8c19d685bf3d79240fbff0783192e2
abbas Rezaei <abbasrezai69@gmail.com>
2020-05-03 06:06:57 +0430

    weather final

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,8 +2,12 @@ const requset = require("request");
 
 const forecast = (latitude, logtiude, callback) => {
 	const url =
-		"https://api.darksky.net/forecast/f8228a4f2761e1771ca38d5986a6e3bc/"+latitude +","+logtiude+"?units=si";
-	requset({ url, json: true }, (error, {body}={}) => {
+		"https://api.darksky.net/forecast/f8228a4f2761e1771ca38d5986a6e3bc/" +
+		latitude +
+		"," +
+		logtiude +
+		"?units=si";
+	requset({ url, json: true }, (error, { body } = {}) => {
 		if (error) {
 			callback("unable to conect weather service", undefined);
 		} else if (body.error) {
@@ -14,7 +18,11 @@ const forecast = (latitude, logtiude, callback) => {
 				body.daily.data[0].summary +
 					"It is currently " +
 					body.currently.temperature +
-					" degrees out.There is a " +
+					" degrees out.This high today is " +
+					body.daily.data[0].temperatureHigh +
+					" with a low of "+
+					body.daily.data[0].temperatureLow+
+					". There is a " +
 					body.currently.precipProbability +
 					"% chance of rain"
 			);
